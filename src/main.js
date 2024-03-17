7e3175948594597152c0ff70ee745ce9d871b0af
automatic <23273891+automaticfood@users.noreply.github.com>
2024-03-17 23:00:51 +0800

    修改提示词

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ const JSONStream = require('JSONStream');
 const app = express();
 const port = 3000;
 
-// JSON数据文件路径
+// 城市列表 JSON 数据文件路径
 const cityList = './src/api/city/all.json';
+const hotSuggests = './src/api/home/hotSuggests.json';
+
 
 // 读取JSON数据文件
-const readJsonFile = () => {
+const readJsonFile = (jsonFile) => {
   try {
-    const data = fs.readFileSync(cityList, 'utf8');
+    const data = fs.readFileSync(jsonFile, 'utf8');
     return JSON.parse(data);
   } catch (error) {
     console.error('Error reading JSON file:', error.message);
@@ -22,7 +24,14 @@ const readJsonFile = () => {
 app.get('/api/city/all', (req, res) => {
   // 启用 CORS
   res.header('Access-Control-Allow-Origin', '*');
-  const jsonData = readJsonFile();
+  const jsonData = readJsonFile(cityList);
+  res.json(jsonData);
+});
+
+app.get('/api/home/hotSuggests', (req, res) => {
+  // 启用 CORS
+  res.header('Access-Control-Allow-Origin', '*');
+  const jsonData = readJsonFile(hotSuggests);
   res.json(jsonData);
 });
 
